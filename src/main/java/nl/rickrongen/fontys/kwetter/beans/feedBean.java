76f7b307a82c1d067e682f9ad8a1d930014e1e64
Rick Rongen <rickrongen@outlook.com>
2017-03-28 21:58:28 +0200

    Added the user to the feedBean Added some XML Namespaces to index.html Added some translated placeholders to the inputs Added personal information

--- src/main/java/nl/rickrongen/fontys/kwetter/beans/feedBean.java
+++ src/main/java/nl/rickrongen/fontys/kwetter/beans/feedBean.java
@@ -1,6 +1,7 @@
 package nl.rickrongen.fontys.kwetter.beans;
 
 import nl.rickrongen.fontys.kwetter.Domain.Kwet;
+import nl.rickrongen.fontys.kwetter.Domain.User;
 import nl.rickrongen.fontys.kwetter.Service.KwetterService;
 
 import javax.enterprise.context.RequestScoped;
@@ -19,12 +20,28 @@ public class feedBean {
     @Inject
     private KwetterService service;
 
+    private String username;
+    private User user;
+
+    public feedBean() {
+        username = FacesContext.getCurrentInstance().getExternalContext().getUserPrincipal().getName();
+    }
+
     public List<Kwet> getFeed(){
-        String username = FacesContext.getCurrentInstance().getExternalContext().getUserPrincipal().getName();
-        return service.getUserFeed(service.getUser(username), 0, 10);  //todo improve
+        return service.getUserFeed(getUser(), 0, 10);  //todo improve
     }
 
     public List<String> getTrendingTags(){
         return service.getTrendingKwets();
+    }
+
+    public User getUser() {
+        if(user == null)
+            user = service.getUser(username);
+        return user;
+    }
+
+    public String getUsername(){
+        return username;
     }
 }
